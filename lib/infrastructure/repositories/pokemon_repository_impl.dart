f03ff3f2a453bd18aa027fdee64b98f61946eb24
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-07 15:12:06 -0700

    FutureProvider - Obtener información del Pokémon

--- lib/infrastructure/repositories/pokemon_repository_impl.dart
+++ lib/infrastructure/repositories/pokemon_repository_impl.dart
@@ -1,9 +1,11 @@
 import 'package:miscelaneos/domain/domain.dart';
+import 'package:miscelaneos/infrastructure/datasources/pokemons_datasource_impl.dart';
 
 class PokemonsRepositryImpl implements PokemonsRepository {
   final PokemonsDatasource datasource;
 
-  PokemonsRepositryImpl(this.datasource);
+  PokemonsRepositryImpl({PokemonsDatasource? datasource})
+      : datasource = datasource ?? PokemonsDatasourceImpl();
 
   @override
   Future<(Pokemon?, String)> getPokemon(String id) async {
