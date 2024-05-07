82d28c88f634147efce1a355e2564f66c73623ee
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-07 11:03:31 -0700

    actions configured

--- lib/config/plugins/quick_actions_plugin.dart
+++ lib/config/plugins/quick_actions_plugin.dart
@@ -1,5 +1,7 @@
 import 'package:quick_actions/quick_actions.dart';
 
+import '../config.dart';
+
 class QuickActionsPlugin {
   static registerActions() {
     const QuickActions quickActions = QuickActions();
@@ -7,12 +9,16 @@ class QuickActionsPlugin {
     quickActions.initialize((shortcutType) {
       switch (shortcutType) {
         case 'biometric':
+          router.push('/biometrics');
           break;
         case 'compass':
+          router.push('/compass');
           break;
         case 'pokemons':
+          router.push('/pokemons');
           break;
         case 'charmander':
+          router.push('/pokemons/4');
           break;
       }
     });
